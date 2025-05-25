09cc8b19ffc631d7b7012d6114575832e5011595
SanchitBahl <sanchitb2002@gmail.com>
2025-05-25 15:43:33 +0530

    Added Modal image support

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import ModalImage from "react-modal-image";
+import './App.css'  
 
 function App() {
   return (
@@ -17,29 +18,31 @@ function App() {
             <td>TV Unit<br/>INR 2000</td>
             <td>TV Unit with storage (LxBxW)</td>
             <td>
-              <img src="https://i.imgur.com/hvYicUM.jpeg" alt="TV Unit" className="w-32 h-32 object-cover" />
-              <img src="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32 object-cover" />
+              <div>
+                <ModalImage small="https://i.imgur.com/hvYicUM.jpeg" large="https://i.imgur.com/hvYicUM.jpeg" alt="TV Unit" className="w-32 h-32"/>
+                <ModalImage small="https://i.imgur.com/PdTxbXw.jpeg" large="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32"/>
+              </div>
             </td>
           </tr>
           <tr>
             <td>Dining Table<br/>INR 3000</td>
             <td>Dining Table with 4 chairs (LxBxW)</td>
             <td>
-              <img src="https://i.imgur.com/FoqpFAa.jpeg" alt="Dining Table" className="w-32 h-32 object-cover" />
+              <ModalImage small="https://i.imgur.com/FoqpFAa.jpeg" large="https://i.imgur.com/FoqpFAa.jpeg" alt="Dining Table" className="w-32 h-32" />
             </td>
           </tr>
           <tr>
             <td>CAT Bookset (Set of 3)<br/>INR 1000</td>
             <td>CAT Quantitative Aptitude by McGrawHill<br/>CAT Verbal Ability & Reading Comprehension by McGrawHill<br/>CAT Data Interpretation & Logical Reasoning by McGrawHill</td>
             <td>
-              <img src="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32 object-cover" />
+              <ModalImage small="https://i.imgur.com/PdTxbXw.jpeg" large="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32" />
             </td>
           </tr>
           <tr>
             <td>CAT Bookset (Set of 3)<br/>INR 1000</td>
             <td>CAT Quantitative Aptitude by McGrawHill<br/>CAT Verbal Ability & Reading Comprehension by McGrawHill<br/>CAT Data Interpretation & Logical Reasoning by McGrawHill</td>
             <td>
-              <img src="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32 object-cover" />
+              <ModalImage small="https://i.imgur.com/PdTxbXw.jpeg" large="https://i.imgur.com/PdTxbXw.jpeg" alt="TV Unit" className="w-32 h-32" />
             </td>
           </tr>
         </tbody>
